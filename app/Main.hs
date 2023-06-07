044947fe7df10b11ad13da2dd4921c2166d54178
Aneesh <aneesh1701@gmail.com>
2023-06-07 14:29:19 +0530

    added primitive numeric functions

--- app/Main.hs
+++ app/Main.hs
@@ -63,6 +63,36 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives =
+  [ ("+", numericBinop (+)),
+    ("-", numericBinop (-)),
+    ("*", numericBinop (*)),
+    ("/", numericBinop div),
+    ("mod", numericBinop mod),
+    ("quotient", numericBinop quot),
+    ("remainder", numericBinop rem)
+  ]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String s) =
+  let parsed = reads s :: [(Integer, String)]
+   in if null parsed
+        then 0
+        else fst $ head parsed
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+-- PRIMITIVE FUNCTIONS --
 
 -- SHOW --
 instance Show LispVal where show = showVal
